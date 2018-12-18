b2c2b55a1464581fa29e8eaa5199f103ffc1e95a
yhira <info@nelog.jp>
2018-12-18 21:42:24 +0900

    不要なコードを削除

--- src/blocks.js
+++ src/blocks.js
@@ -9,6 +9,6 @@
  * Webpack is compiling as the input file.
  */
 
-import './block/block.js';
-import './edit-block/block.js';
+// import './block/block.js';
+// import './edit-block/block.js';
 import './info-block/block.js';
