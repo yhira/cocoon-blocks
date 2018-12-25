95567ad00abdb5c0d43b319e5a6a1159c8746cc0
yhira <info@nelog.jp>
2018-12-25 16:28:28 +0900

    余計なエラーメッセージを表示させない

--- plugin-update-checker/Puc/v4p4/Metadata.php
+++ plugin-update-checker/Puc/v4p4/Metadata.php
@@ -32,7 +32,7 @@ if ( !class_exists('Puc_v4p4_Metadata', false) ):
 			if ( empty($apiResponse) || !is_object($apiResponse) ){
 				$errorMessage = "Failed to parse update metadata. Try validating your .json file with http://jsonlint.com/";
 				do_action('puc_api_error', new WP_Error('puc-invalid-json', $errorMessage));
-				trigger_error($errorMessage, E_USER_NOTICE);
+				//trigger_error($errorMessage, E_USER_NOTICE);
 				return false;
 			}
 
