2aaa9be2634f5bfec9419ba045fb80ba60ab63cf
yhira <info@nelog.jp>
2018-12-25 15:54:54 +0900

    バージョン変更

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Cocoon Blocks — is a Gutenberg plugin created via create-guten-block.
  * Author: わいひら
  * Author URI: https://nelog.jp/
- * Version: 0.0.1
+ * Version: 0.0.2
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
@@ -24,7 +24,7 @@ require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
 
 //自動アップデート
 require_once plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php';
-$SangoGutenbergUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+$CocoonBlocksUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
     'https://raw.githubusercontent.com/yhira/cocoon-blocks/master/update-info.json',
     __FILE__,
     'cocoon-blocks'
