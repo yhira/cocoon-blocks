d8d6b5de31733e9e1855288208b3733135e08952
yhira <info@nelog.jp>
2018-12-25 16:48:31 +0900

    自動アップデート機能無効テスト

--- plugin.php
+++ plugin.php
@@ -13,6 +13,9 @@
  */
 
 // // Exit if accessed directly.
+// var_dump(ABSPATH);
+// var_dump(__DIR__);
+// var_dump(realpath('./'));
 // if ( !defined( 'ABSPATH' )  ) {
 // 	exit;
 // }
@@ -22,12 +25,12 @@
  */
 require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
 
-//自動アップデート
-require_once plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php';
-$CocoonBlocksUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'https://raw.githubusercontent.com/yhira/cocoon-blocks/master/update-info.json',
-    __FILE__,
-    'cocoon-blocks'
-);
+// //自動アップデート
+// require_once plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php';
+// $CocoonBlocksUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+//     'https://raw.githubusercontent.com/yhira/cocoon-blocks/master/update-info.json',
+//     __FILE__,
+//     'cocoon-blocks'
+// );
 
 
